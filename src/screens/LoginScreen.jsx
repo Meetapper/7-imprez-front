d507da4292f22f769af330d5b47371a991edd431
Berezka Adam <adam.berezka@exorigo-upos.pl>
2023-04-01 16:54:37 +0200

    eas setup

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { styles } from "../constants"
 import { Text, DefaultTheme, Provider as PaperProvider, TextInput } from 'react-native-paper';
 import { View } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 const LoginScreen = () => {
   return (
     <PaperProvider theme={theme}>
       <View style={styles.centeredFlex}>
-        <Text >
-          LoginScreen  GOOGLELELEL
+        <Text theme={theme}>
+          LoginScreen  GOOGLELELELsadasda
         </Text>
       </View>
     </PaperProvider>
@@ -19,7 +24,7 @@ const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'tomato',
+    primary: 'black',
     accent: 'yellow',
   },
 };
