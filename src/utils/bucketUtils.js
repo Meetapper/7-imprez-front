3eb412019272f0b1252cf2c5d428e6fb0433ec00
Patryk Skupien <pskupien@akamai.com>
2023-04-02 03:20:54 +0200

    added support for buckets

---
+++ src/utils/bucketUtils.js
@@ -0,0 +1,55 @@
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
+
+import * as ImagePicker from 'expo-image-picker';
+import { RNS3 } from 'react-native-aws3';
+
+const accessKeyId = "access";
+const secretAccessKey = "secret";
+
+export const getImageUrl = async (username) => {
+    const AWS = require('aws-sdk')
+    const s3 = new AWS.S3()
+    AWS.config.update({
+        region: "eu-central-1",
+        accessKeyId,
+        secretAccessKey,
+        endpoint: "https://eu-central-1.linodeobjects.com",
+    })
+
+    const url = s3.getSignedUrl('getObject', {
+      Bucket: "shareem-photos",
+      Key: username,
+      Expires: 3600
+    })
+    
+    return url;
+}
+
+export const uploadImage = async (username) => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        quality: 1,
+    });
+
+    const file = {
+        uri: result.uri,
+        name: username,
+        type: "image/png",
+      }
+  
+      const options = {
+        bucket: "shareem-photos",
+        region: "eu-central-1",
+        accessKey: accessKeyId,
+        secretKey: secretAccessKey,
+        awsUrl: "eu-central-1.linodeobjects.com"
+      }
+    
+      RNS3.put(file, options).then(response => {
+        if (response.status !== 201)
+          throw new Error("Failed to upload image to S3");
+        console.log(response.body);
+      });
+}
